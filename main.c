bff6245f0f1a9e4d5f7d873667ce6618288f2898
KevinOlvera <kevin.jesus.olvera@gmail.com>
2020-02-27 01:11:35 -0600

    do producers and consumers dinamic

--- main.c
+++ main.c
@@ -8,33 +8,29 @@
 //char global_variable_1[4];
 //char global_variable_2[4];
 
-char global_variable_1;
-char global_variable_2;
+#define lim_cp 1    //# Consumos Producciones
+#define lim_sp 16    //# Semaforos
+#define lim_sc 8    //# Secciones
+#define lim_th 10    //# Hilos
 
-sem_t sem[4];
+char global_variable[lim_sc];
 
-int lim_cp = 1;
-
-int error(char *err);
-
-
+sem_t sem[lim_sp];
 
 void *producer(void *args) {
     char c = *(char *)args;
     int i = 0;
     while (i < lim_cp)
     {
-        if (sem_trywait(&sem[0]) == 0) {
-            global_variable_1 = c;
-            printf("Producer %c - Ciclo %d - Seccion S1\n", global_variable_1, i);
-            sem_post(&sem[1]);
-            i++;
-        } else if (sem_trywait(&sem[2]) == 0) {
-            global_variable_2 = c;
-            printf("Producer %c - Ciclo %d - Seccion S2\n", global_variable_2, i);
-            sem_post(&sem[3]);
-            i++;
-        }
+        int k = 0;
+        for (int j = 0; k < lim_sc; j = j+2, k++)
+            if (sem_trywait(&sem[j]) == 0) {
+                global_variable[k] = c;
+                printf("Producer %c - Ciclo %d\t- Seccion S%d\n", global_variable[k], i+1, k+1);
+                sem_post(&sem[j+1]);
+                i++;
+                break;
+            }
     }
     return EXIT_SUCCESS;
 }
@@ -44,76 +40,60 @@ void *consumer(void *args) {
     int i = 0;
     while (i < lim_cp)
     {
-        if (sem_trywait(&sem[1]) == 0) {
-            printf("Consumer %c - Ciclo %d - Seccion S1 - Valor %c\n", c, i, global_variable_1);
-            sem_post(&sem[0]);
-            i++;
-        } else if (sem_trywait(&sem[3]) == 0) {
-            printf("Consumer %c - Ciclo %d - Seccion S2 - Valor %c\n", c, i, global_variable_2);
-            sem_post(&sem[2]);
-            i++;
-        }
+        int k = 0;
+        for (int j = 0; k < lim_sc; j = j+2, k++)
+            if (sem_trywait(&sem[j+1]) == 0) {
+                printf("Consumer %c - Ciclo %d\t- Seccion S%d - Valor %c\n", c, i+1, k+1, global_variable[k]);
+                sem_post(&sem[j]);
+                i++;
+                break;
+            }
     }
 }
 
-
-
-
 int main(int argc, char const *argv[])
 {
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < lim_sp; i++)
         if (i%2 == 0)
             sem_init(&sem[i], 0, 1);
         else
             sem_init(&sem[i], 0, 0);
 
     //Creacion de hilos
-    pthread_t threads[8];
+    pthread_t threads[lim_th];
 
     if (0 != pthread_create(&threads[0], NULL, producer, (void *)"A"))
-            return -1;
+        return -1;
 
     if (0 != pthread_create(&threads[1], NULL, producer, (void *)"B"))
-            return -1;
+        return -1;
             
     if (0 != pthread_create(&threads[2], NULL, producer, (void *)"C"))
-            return -1;
+        return -1;
 
     if (0 != pthread_create(&threads[3], NULL, producer, (void *)"D"))
-            return -1;
+        return -1;
 
-    if (0 != pthread_create(&threads[4], NULL, consumer, (void *)"1"))
-            return -1;
+    if (0 != pthread_create(&threads[4], NULL, producer, (void *)"E"))
+        return -1;
 
-    if (0 != pthread_create(&threads[5], NULL, consumer, (void *)"2"))
-            return -1;
+    if (0 != pthread_create(&threads[5], NULL, consumer, (void *)"1"))
+        return -1;
+
+    if (0 != pthread_create(&threads[6], NULL, consumer, (void *)"2"))
+        return -1;
             
-    if (0 != pthread_create(&threads[6], NULL, consumer, (void *)"3"))
-            return -1;
+    if (0 != pthread_create(&threads[7], NULL, consumer, (void *)"3"))
+        return -1;
 
-    if (0 != pthread_create(&threads[7], NULL, consumer, (void *)"4"))
-            return -1;
+    if (0 != pthread_create(&threads[8], NULL, consumer, (void *)"4"))
+        return -1;
 
-    pthread_join(threads[0], NULL);
-    pthread_join(threads[1], NULL);
-    pthread_join(threads[2], NULL);
-    pthread_join(threads[3], NULL);
-    pthread_join(threads[4], NULL);
-    pthread_join(threads[5], NULL);
-    pthread_join(threads[6], NULL);
-    pthread_join(threads[7], NULL);
+    if (0 != pthread_create(&threads[9], NULL, consumer, (void *)"5"))
+        return -1;
 
-
-
-
-
-
-
+    for (int i = 0; i < lim_th; i++)
+        pthread_join(threads[i], NULL);
 
     return 0;
-}
-
-int error(char *err) {
-    perror(err);
-    return -1;
 }
